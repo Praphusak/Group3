302bff4082376e248868c8753b0752c103ca100d
Searcheulong <sear_cheulong@cmu.ac.th>
2017-03-12 05:15:51 +0700

    update calculate method for give grade U

--- src/main/java/utility/GradeCalculator.java
+++ src/main/java/utility/GradeCalculator.java
@@ -7,13 +7,17 @@ public class GradeCalculator {
     public char calculate(double... scores){
         double totalScore=scores[0]+scores[1]+scores[2];
         char grade='\0';
-        if(totalScore>95.0) {
-            grade = 'A';
-        } else if(totalScore<=94&&totalScore>80) {
-            grade = 'B';
-        }else if(totalScore<=80&&totalScore>60) {
-            grade = 'C';
-        }else grade='F';
+        if (scores[0]<0||scores[1]<0||scores[2]<0){
+            grade='U';
+        }else {
+            if (totalScore > 95.0) {
+                grade = 'A';
+            } else if (totalScore <= 94 && totalScore > 80) {
+                grade = 'B';
+            } else if (totalScore <= 80 && totalScore > 60) {
+                grade = 'C';
+            } else grade = 'F';
+        }
         return grade;
     }
 }
