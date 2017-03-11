86b92553bde441d547ad90dab688a08371c90983
jariyaporn <jariyaporn_k@cmu.ac.th>
2017-03-11 13:31:12 +0700

    Add Currency stub file

---
+++ src/main/java/utility/Currency.java
@@ -0,0 +1,10 @@
+package utility;
+
+/**
+ * Created by CHEULONG on 3/11/2017.
+ */
+public class Currency {
+    public double exchange(String sourceCurrrency,double amount,String targetCurrency){
+        return 0.0;
+    }
+}
