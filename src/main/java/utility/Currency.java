e825af2d6faeeb35738cd6cc039344abdffa8ec4
praphusak <praphusak_t@cmu.ac.th>
2017-03-12 02:47:27 +0700

    update exchage method  for target currency is USD

--- src/main/java/utility/Currency.java
+++ src/main/java/utility/Currency.java
@@ -6,11 +6,11 @@ package utility;
 public class Currency {
     public double exchange(String sourceCurrrency,double amount,String targetCurrency){
         double exchangeCurrency=0.0;
-        if(!sourceCurrrency.equalsIgnoreCase("USD")&&(!targetCurrency.equalsIgnoreCase("USD"))){
-            if (sourceCurrrency.equalsIgnoreCase("THB")) {
-                 exchangeCurrency=(amount/33)*1.01;
-            }else  exchangeCurrency=(amount/1.01)*33;
-        }
+        // source currency is THB
+        if(sourceCurrrency.equalsIgnoreCase("THB")){
+           exchangeCurrency=amount/33;
+            // source currency is EUR
+        }else exchangeCurrency=amount/1.01;
         return exchangeCurrency;
     }
 }
