583294989c0be146203cf69644571210c619fed3
praphusak <praphusak_t@cmu.ac.th>
2017-03-12 04:44:22 +0700

    add calculate method for giving grade B

--- src/main/java/utility/GradeCalculator.java
+++ src/main/java/utility/GradeCalculator.java
@@ -5,6 +5,11 @@ package utility;
  */
 public class GradeCalculator {
     public char calculate(double... scores){
-        return '\0';
+        double totalScore=scores[0]+scores[1]+scores[2];
+        char grade='\0';
+        if(totalScore<=94&&totalScore>80) {
+            grade = 'B';
+        }
+        return grade;
     }
 }
