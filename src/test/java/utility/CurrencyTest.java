25aff22c8377aa1758891cd6c509fd73db0fa322
praphusak <praphusak_t@cmu.ac.th>
2017-03-12 05:34:22 +0700

    add test case for currency state out of table

--- src/test/java/utility/CurrencyTest.java
+++ src/test/java/utility/CurrencyTest.java
@@ -19,6 +19,6 @@ public class CurrencyTest {
         assertThat(currency.exchange("USD",10.0,"THB"),closeTo(330.0,0.001));
         assertThat(currency.exchange("EUR",100.0,"USD"),closeTo(99.009,0.001));
         assertThat(currency.exchange("USD",10.0,"EUR"),closeTo(10.1,0.001));
-
+        assertThat(currency.exchange("SGD",100.0,"USD"),closeTo(Double.NaN,0.001));
     }
 }
